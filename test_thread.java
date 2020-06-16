787a53ecc42fe7eac1d5e3b883bd689a87cc0682
CT <uhjio1233@gmail.com>
2020-06-16 22:38:06 +0800

    hourse

--- test_thread.java
+++ test_thread.java
@@ -1,9 +1,36 @@
-
+class Horse extends Thread {
+    // 覆寫Thread方法run()
+    public void run() {
+            try {
+                sleep(1000);
+                System.out.println(getName()+"finished");
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                System.out.println(getName()+"error");
+            }
+    }
+  }
 public class test_thread{
     public static void main(String[] args){
-        Thread thr = Thread.currentThread();
-        System.out.println("目前執行緒名稱:"+thr.getName());
-        thr.setName("DEMO");
-        System.out.println("更改後的名稱:"+thr.getName());
+       
+        //產生Horse物件並啟動執行緒
+        Horse h1 = new Horse();
+        Horse h2 = new Horse();
+        Horse h3 = new Horse();
+        h1.setName("1");
+        h2.setName("2");
+        h3.setName("3");
+        h1.start();
+        h2.start();
+        h3.start();
+        try{
+            h1.join();
+            h2.join();
+            h3.join();
+        }catch(InterruptedException e){
+            System.out.println("main error");
+        }
+        System.out.println("all finished");
+        
     }
 }
